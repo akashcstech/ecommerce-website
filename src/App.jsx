8dd4c95bf6672141d4af1be944b23d29603cabd2
akashcstech <akash.cstech@gmail.com>
2026-08-31 09:40:04 +0530

    added checkoutpage and orderpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { HomePage } from './pages/HomePage'
+import { Routes, Route } from 'react-router';
+import {Orders} from './pages/Orders'
+import { HomePage } from './pages/HomePage';
+import { CheckOutPage } from './pages/CheckOutPage';
 import './App.css'
 
 function App() {
 
   return (
-    <>
-      <HomePage />
-    </>
-  )
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+      <Route path='Orders' element={<Orders />} />
+      <Route path='CheckOut' element={<CheckOutPage />} />
+    </Routes>
+  );
 }
 
 export default App
--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -5,7 +5,7 @@ export function Orders(){
             <title>Orders</title>
             <div className="header">
             <div className="left-section">
-                <a href="index.html" className="header-link">
+                <a href="/" className="header-link">
                 <img className="logo"
                     src="images/logo-white.png" />
                 <img className="mobile-logo"
@@ -22,12 +22,12 @@ export function Orders(){
             </div>
 
             <div className="right-section">
-                <a className="orders-link header-link" href="orders.html">
+                <a className="orders-link header-link" href="Orders">
 
                 <span className="orders-text">Orders</span>
                 </a>
 
-                <a className="cart-link header-link" href="checkout.html">
+                <a className="cart-link header-link" href="CheckOut">
                 <img className="cart-icon" src="images/icons/cart-icon.png" />
                 <div className="cart-quantity">3</div>
                 <div className="cart-text">Cart</div>
@@ -81,7 +81,7 @@ export function Orders(){
                     </div>
 
                     <div className="product-actions">
-                    <a href="tracking.html">
+                    <a href="Tracking">
                         <button className="track-package-button button-secondary">
                         Track package
                         </button>
@@ -109,7 +109,7 @@ export function Orders(){
                     </div>
 
                     <div className="product-actions">
-                    <a href="tracking.html">
+                    <a href="Tracking">
                         <button className="track-package-button button-secondary">
                         Track package
                         </button>
@@ -160,7 +160,7 @@ export function Orders(){
                     </div>
 
                     <div className="product-actions">
-                    <a href="tracking.html">
+                    <a href="Tracking">
                         <button className="track-package-button button-secondary">
                         Track package
                         </button>
